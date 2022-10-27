e736f2cd936422a0ef74a33ac546d4f0677ee3fe
minhtri2908 <91834683+minhtri2908@users.noreply.github.com>
2022-10-27 21:05:33 +0700

    FE

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './page/home';
+import Login from './page/login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
