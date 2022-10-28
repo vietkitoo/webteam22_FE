fb33ce01e128665184c4385b82a8a68f063fb927
chetung2001 <91051353+chetung2001@users.noreply.github.com>
2022-10-28 09:53:10 +0700

    Add fortawesome
    Add icon

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './styles/css/bootstrap.min.css';
+import './styles/css/App.css';
+import Login from './page/login/login';
+import Home from './page/home/home';
+import { useContext } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <main className="App">
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>,
+    </main>
+  );
+}
+
+export default App;
