7bc80898f14bbc020922f17a56690ec9d0dffc8d
minhtri2908 <91834683+minhtri2908@users.noreply.github.com>
2022-11-09 09:50:49 +0700

    update

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -4,10 +4,16 @@ import '../../styles/css/App.css';
 import hinh1 from '../../image/173281189.jpg';
 import hinh2 from '../../image/1651729679291.jpg';
 import hinh3 from '../../image/hinh1_dxxx.jpg';
-import Carousel from 'react-bootstrap/Carousel';
 import { Link } from 'react-router-dom';
-import { faBed, faCamera, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBed,
+  faCamera,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function home() {
   return (
     <div>
@@ -18,12 +24,12 @@ function home() {
           </a>
           <form className="form-inline">
             <Link to="../register-hotel">
-            <button
-              type="button"
-              className="btn btn-outline-info color_hover_btn_outline_info"
-            >
-              Đăng thông tin phòng nghỉ của quý khách
-            </button>
+              <button
+                type="button"
+                className="btn btn-outline-info color_hover_btn_outline_info"
+              >
+                Đăng thông tin phòng nghỉ của quý khách
+              </button>
             </Link>
             <Link to="../login">
               <button type="button" className="btn btn-light btn_sign">
@@ -34,95 +40,126 @@ function home() {
         </nav>
 
         <nav className="navbar navbar-light justify-content-center">
-        <Link to='/'>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-           
-          >
-            <FontAwesomeIcon icon={faBed} />
-          
-            Lưu Trú
-          </button>
-          </Link>  
-          <Link to='/'>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-          >
-            <FontAwesomeIcon icon={faPlane} />
-            Chuyến Bay
-          </button>
-          </Link>  
-          <Link to='/'>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-          >
-            <FontAwesomeIcon icon={faBed} />
-            Chuyến Bay + Khách sạn
-          </button>
-          </Link>  
-          <Link to='/'>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-          >
-            <FontAwesomeIcon icon={faCar} />
-
-            Thuê xe
-          </button>
-          </Link>  
-          <Link to='/ '>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-          >
-            <FontAwesomeIcon icon={faCamera} />
-            
-            Địa điểm tham quan
-          </button>
-          </Link>  
-          <Link to='/'>
-          <button
-            type="button"
-            className="btn btn-light button-navbar btn_sign btn_select_list"
-          >
-            <FontAwesomeIcon icon={faTaxi } />
-            Taxi sân bay
-          </button>
-          </Link>  
-       
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faBed} />
+              Lưu Trú
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faPlane} />
+              Chuyến Bay
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faBed} />
+              Chuyến Bay + Khách sạn
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faCar} />
+              Thuê xe
+            </button>
+          </Link>
+          <Link to="/ ">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faCamera} />
+              Địa điểm tham quan
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-light button-navbar btn_sign btn_select_list"
+            >
+              <FontAwesomeIcon icon={faTaxi} />
+              Taxi sân bay
+            </button>
+          </Link>
         </nav>
       </header>
       <div className="contener">
-        <Carousel>
-          <Carousel.Item>
-            <img className="d-block w-100" src={hinh1} alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={hinh2} alt="Second slide" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={hinh3} alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-bs-ride="true"
+        >
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={hinh1} alt="First slide" className="d-block w-100" />
+            </div>
+            <div className="carousel-item">
+              <img src={hinh2} alt="Second slide" className="d-block w-100" />
+            </div>
+            <div className="carousel-item">
+              <img src={hinh3} alt="Third slide" className="d-block w-100" />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
         <div className="info_select">
           <div className="text_1">Áp dụng đến 3/1/2023</div>
           <div className="text_2">Tiết kiệm 15% với Ưu Đãi Cuối Năm</div>
