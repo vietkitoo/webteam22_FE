57046276abf05e7c586533745552a2bb3054da5b
minhtri2908 <91834683+minhtri2908@users.noreply.github.com>
2022-11-10 10:06:24 +0700

    update 10/11/2022

--- src/page/register-hotel/regishotel.js
+++ src/page/register-hotel/regishotel.js
@@ -1,6 +1,7 @@
 import './regishotel.css';
 import '../../styles/css/bootstrap.min.css';
 import '../../styles/css/App.css';
+import register_image from '../../image/register-partner.png';
 import { FcCheckmark } from 'react-icons/fc';
 import { BsCheckCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
@@ -94,54 +95,57 @@ function regishotel() {
           </div>
         </div>
       </div>
-      <div className="infor-for-partner justify-content-center d-grid">
-        <div className="fw-bold fs-3
-        
-         ">
+      <div className="infor-for-partner justify-content-center d-grid offset-lg-2 ps-5 ">
+        <div className=" navbar-text fw-bold fs-2">
           Sự an tâm của Quý vị là ưu tiên hàng đầu của chúng tôi
         </div>
-        <div>
+        <div className="navbar-text">
           Đây là cách chúng tôi giúp Quý vị cảm thấy tự tin chào đón khách:
         </div>
-        <div className="regulation-for-partner">
-          <div className='w-50'>
-            <div className="navbar-text d-flex">
-              <BsCheckCircle />
-              <div className="d-block mx-1">
-                Thiết lập<strong> {" "}quy tắc chung {'  '}</strong> {' '} mà khách
-                phải chấp thuận trước khi đến lưu trú
+        <div className="regulation-for-partner d-flex ">
+          <div className="col-6">
+            <div className="navbar-text d-flex py-3">
+              <BsCheckCircle className="width1" />
+              <div className="d-block mx-2">
+                Thiết lập<strong> quy tắc chung {'  '}</strong> mà khách phải
+                chấp thuận trước khi đến lưu trú
               </div>
             </div>
-            <div className="navbar-text d-flex">
-              <BsCheckCircle />
-              <div className="d-block mx-1">
-                Yêu cầu {' '}<strong> đặt cọc đề phòng hư hại </strong>
-                {' '}để an tâm hơn
+            <div className="navbar-text d-flex py-3">
+              <BsCheckCircle className="width1" />
+              <div className="d-block mx-2">
+                Yêu cầu <strong> đặt cọc đề phòng hư hại </strong> để an tâm hơn
               </div>
             </div>
-            <div className="navbar-text d-flex">
-              <BsCheckCircle />
-              <div className="d-block mx-1">
-                <strong>
-                  Báo cáo hành vi sai phạm của khách
-                </strong>{' '}
-                nếu có vấn đề xảy ra
+            <div className="navbar-text d-flex py-3">
+              <BsCheckCircle className="width1" />
+              <div className="d-block mx-2">
+                <strong>Báo cáo hành vi sai phạm của khách</strong> nếu có vấn
+                đề xảy ra
               </div>
             </div>
-            <div className="navbar-text d-flex">
-              <BsCheckCircle />
-              <div className="d-block mx-1" >
-                Được {' '}<strong>hỗ trợ 24/7</strong>{' '} với hơn 43 ngôn ngữ
+            <div className="navbar-text d-flex py-3">
+              <BsCheckCircle className="width1" />
+              <div className="d-block mx-2">
+                Được <strong>hỗ trợ 24/7</strong> với hơn 43 ngôn ngữ
               </div>
             </div>
-            <div className="navbar-text d-flex">
-              <BsCheckCircle className='width1' />
-              <div className='mx-1'>
+            <div className="navbar-text d-flex py-3">
+              <BsCheckCircle className="width1" />
+              <div className="mx-1 w-75">
                 Được bảo vệ trước các yêu cầu bồi thường liên quan đến trách
                 nhiệm từ khách và cư dân xung quanh lên đến 1.000.000 USD cho
                 mỗi đơn đặt
               </div>
             </div>
+            <div className="py-3">
+              <button type="button" className="btn btn-outline-primary ">
+                Tìm Hiểu Thêm
+              </button>
+            </div>
+          </div>
+          <div className=" ">
+            <img className="col-10 " src={register_image} alt="register" />
           </div>
         </div>
       </div>
